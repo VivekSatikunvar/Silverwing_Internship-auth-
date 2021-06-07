20db1936f59a9a8b449850a7be0719bbf908141e
Vivek Satikunvar <viveksatikunvar142@gmail.com>
2021-06-07 22:03:10 +0530

    installed allauth and setup template

---
+++ authentication/Home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+from django.http import HttpResponse
+
+
+# Create your views here.
+def hello(request):
+    return HttpResponse('hello amigos!') 
+
+
+def home(request):
+    return render(request,'index.html')
