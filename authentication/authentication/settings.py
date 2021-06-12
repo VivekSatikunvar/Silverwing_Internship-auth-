ca27711eafc592cd72e69c66b58d66f8cc912043
Vivek Satikunvar <viveksatikunvar142@gmail.com>
2021-06-12 13:45:15 +0530

    updated setting.py

--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -184,4 +184,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'viveksatikunvar142@gmail.com'
-EMAIL_HOST_PASSWORD = '9662888265'
+EMAIL_HOST_PASSWORD = '**********'
